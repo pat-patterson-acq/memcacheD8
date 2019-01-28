ba6b8d82d6d7c48a1a23a6c947f9f1a0ed253bfc
Pat Patterson <pat.patterson@acquia.com>
2019-01-28 16:21:41 -0500

    ACSF doesn't have a sites/all directory

--- settings-memcached.php
+++ settings-memcached.php
@@ -1,2 +1,2 @@
 // Include code to enable memcache for Acquia Cloud sites.
-include $app_root . '/sites/all/settings-acquia-cloud-memcache-8.x-2.0.php';
+include $app_root . '/sites/default/settings-acquia-cloud-memcache-8.x-2.0.php';
